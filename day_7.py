76c9a8b3649182388ed0d49899c7a0846cf75451
Domenic Di Francesco <domenicdifrancesco@gmail.com>
2022-05-15 20:23:34 +0100

    adding day 7 solution

---
+++ day_7.py
@@ -0,0 +1,7 @@
+import os, pandas as pd, numpy as np
+
+os.chdir(path = "/Users/ddifrancesco/OneDrive - The Alan Turing Institute/AoC")
+os.listdir()
+
+with open("day_6.txt") as file:
+    data = file.readline()
